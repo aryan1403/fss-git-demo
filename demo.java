59b76b98433763b6739d1e7feb59906bec5686dc
aryan1403 <aaryan14032006@email.com>
2025-01-29 11:02:30 +0530

    feature added

--- demo.java
+++ demo.java
@@ -1,4 +1,7 @@
 public class demo {
+    void print1() {
+        System.out.println("feature code");
+    }
     public static void main(String[] args) {
         System.out.println("hello master");
     }
