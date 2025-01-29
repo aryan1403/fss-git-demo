cf9243475f06a61360fa1bbe1974e71a01f12a8b
aryan1403 <aaryan14032006@email.com>
2025-01-29 11:06:23 +0530

    feature added

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo {
-    void print1() {
-        System.out.println("feature code");
+    public static void sum(int a, int b) {
+        System.out.println(a + b);
     }
     public static void main(String[] args) {
         System.out.println("hello master");
